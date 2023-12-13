4f2c7a5f6789a1336c524ea1f5714ed3d9357aa6
Елизавета Асеева <aseyshaaa@mail.ru>
2023-12-13 23:19:34 +0700

    first

---
+++ main.py
@@ -0,0 +1,63 @@
+from lorem_text import lorem
+
+
+def analysis_text(text):
+    # словарь для хранения частот символов
+    frequency = {}
+    # Подсчитываем общее число символов в тексте
+    count_symbols = len(text)
+    # Проходимся по каждому символу в тексте
+    for symbol in text:
+        if symbol in frequency:
+            frequency[symbol] += 1
+        else:
+            frequency[symbol] = 1
+    # Преобразуем частоты символов в пары (символ, частота встречаемости)
+    result = [(symbol, frequency / count_symbols) for symbol, frequency in frequency.items()]
+    return result
+
+
+def hand_text():
+    text = input("Введите текст: ")
+    print("Вы ввели следующий текст:")
+    print(text)
+    return text
+
+
+def autotext():
+    text = lorem.paragraph()
+    print(text)
+    return text
+
+
+def menu():
+    text = ""
+    while True:
+        print("Главное меню:")
+        print("1. Ввести текст вручную.")
+        print("2. Ввести текст случайным образом.")
+        print("3. Решение задачи.")
+        print("4. Выход из программы ")
+
+        choose = input("Выберите пункт меню: ")
+
+        if choose == "1":
+            text = hand_text()
+            input("Нажмите Enter, чтобы продолжить...")
+        elif choose == "2":
+            text = autotext()
+            input("Нажмите Enter, чтобы продолжить...")
+        elif choose == "3":
+            result = analysis_text(text)
+            for symbol, frequency in result:
+                print(f"{symbol} - {frequency:.4f}")
+            input("Нажмите Enter, чтобы продолжить...")
+        elif choose == "4":
+            print("Выход из программы.")
+            break
+        else:
+            print("Некорректный выбор. Пожалуйста, выберите снова.")
+
+
+if __name__ == "__main__":
+    menu()

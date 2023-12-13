ad7c2f9471f2b985594a40a6b8bfe31ecc4af11d
Елизавета Асеева <aseyshaaa@mail.ru>
2023-12-13 23:23:20 +0700

    create class boss

--- main.py
+++ main.py
@@ -21,12 +21,25 @@ class Worker:
 
 
 class Boss:
-    def __init__(self):
-        return
+    def __init__(self, name, surname, age):
+        self.__name = name
+        self.__surname = surname
+        self.__age = age
+
+    @property
+    def name(self):
+        return self.__name
+
+    @property
+    def surname(self):
+        return self.__surname
+
+    @property
+    def age(self):
+        return self.__age
 
     def __str__(self):
-        return
-
+        return f"{self.name} {self.surname}, Возраст: {self.age}"
 
 class Work:
     def __init__(self):

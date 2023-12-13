c9bf9e58d15ef91b4a0c9d45a6d6c858c168bfe9
Елизавета Асеева <aseyshaaa@mail.ru>
2023-12-13 23:24:37 +0700

    create class work

--- main.py
+++ main.py
@@ -41,9 +41,35 @@ class Boss:
     def __str__(self):
         return f"{self.name} {self.surname}, Возраст: {self.age}"
 
+
 class Work:
-    def __init__(self):
-        return
+    def __init__(self, name_of_work):
+        self.__name_of_work = name_of_work
+        self.__boss = None
+        self.__workers = []
+
+    @property
+    def name_of_work(self):
+        return self.__name_of_work
+
+    @property
+    def boss(self):
+        return self.__boss
+
+    @property
+    def workers(self):
+        return self.__workers
+
+    def make_boss(self, boss):
+        self.__boss = boss
+
+    def make_worker(self, worker):
+        self.__workers.append(worker)
+
+    def __str__(self):
+        boss_str = f"Директор: {self.boss}" if self.boss else "Директор не назначен"
+        workers_str = "\n".join(str(worker) for worker in self.workers)
+        return f"Фирма: {self.name_of_work}\n{boss_str}\nСотрудники:\n{workers_str}"
 
 
 def create_work():
